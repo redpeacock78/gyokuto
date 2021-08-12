6c7d0655f75c37eec2828370a8045136b821b655
redpeacock78 <scarletpeacock78@gmail.com>
2021-08-13 00:42:19 +0900

    feat: Add src/modules/tools/*.ts

---
+++ src/modules/tools/shuffle.ts
@@ -0,0 +1,14 @@
+//シャッフル
+import { _shuffle } from '#types/main';
+
+const shuffle: _shuffle = <T>(array: T[]): T[] => {
+  const result: T[] = [...array];
+  let i: number = result.length;
+  for (i; 1 < i; i--) {
+    const j: number = Math.floor(Math.random() * i);
+    [result[j], result[i - 1]] = [result[i - 1], result[j]];
+  }
+  return result;
+};
+
+export = shuffle;
